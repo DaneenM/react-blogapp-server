8c5aa2b0419e053d0a2b0036d0115f751ba4ba1e
DaneenM <45468867+DaneenM@users.noreply.github.com>
2023-02-16 18:52:03 -0500

    fix typo

--- routes/posts.js
+++ routes/posts.js
@@ -1,45 +1,45 @@
 const express = require("express");
 const router = express.Router();
 
-//Creat a post
-router.post("/posts", (req,res) => {
+//Create a post
+router.post("/posts", (req, res) => {
   Post.create(req.body)
-    .then(post => res.json(post))
-    .catch(err => res.status(400).json(err));
+    .then((post) => res.json(post))
+    .catch((err) => res.status(400).json(err));
 });
 
 //Get all posts
-router.get("/posts", (req,res) => {
-    Post.findAll()
-      .then(post => res.json(posts))
-      .catch(err => res.status(400).json(err));
-  });
+router.get("/posts", (req, res) => {
+  Post.findAll()
+    .then((post) => res.json(posts))
+    .catch((err) => res.status(400).json(err));
+});
 
 //Get a single post
-router.get("/posts/:id", (req,res) => {
-    Post.findByPk(req.params.id)
-      .then(post => res.json(post))
-      .catch(err => res.status(400).json(err));
-  });  
+router.get("/posts/:id", (req, res) => {
+  Post.findByPk(req.params.id)
+    .then((post) => res.json(post))
+    .catch((err) => res.status(400).json(err));
+});
 
-  //Update a post
-  router.put("/posts/:id", (req,res) => {
-    Post.update(req.body, {
-        where: {
-            id:req.params.id
-        }
-    })
-      .then(post => res.json(post))
-      .catch(err => res.status(400).json(err));
-  });
+//Update a post
+router.put("/posts/:id", (req, res) => {
+  Post.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((post) => res.json(post))
+    .catch((err) => res.status(400).json(err));
+});
 
 //Delete a post
-router.delete("/posts/:id", (req,res) => {
-    Post.destroy({
-        where: {
-            id:req.params.id
-        }
-    })
-      .then(post => res.json(post))
-      .catch(err => res.status(400).json(err));
-  });
+router.delete("/posts/:id", (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((post) => res.json(post))
+    .catch((err) => res.status(400).json(err));
+});
